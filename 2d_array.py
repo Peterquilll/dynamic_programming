141547a58355d46e6cc14294901001fe33cc3902
Domingo Hernandez <domingohernandez@Domingos-MacBook-Pro.local>
2018-05-29 15:52:45 -0500

    Dynamic progams involving stocks

---
+++ 2d_array.py
@@ -0,0 +1,20 @@
+def main():
+    # choice you'll be making
+    # also you'll need a "seed" choice which is usually row
+
+    num_rows = 4
+    num_cols = 7
+
+    t = []
+    for i in range(num_rows):
+        t.append([0 for j in range(num_cols)])
+
+        for i in range (len(t)):
+            for j in range(len(t[0])):
+                current = t[i][j]
+                print(current, end=" ")
+
+            print('')
+
+if __name__ == "__main__":
+    main()
